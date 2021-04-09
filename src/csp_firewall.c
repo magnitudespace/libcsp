aa7c2fb0e0ec5867d4a2f58c6a133758830d7021
Jens Hertz <jens.hertz@magnitudespace.com>
2021-04-09 17:12:11 +0200

    Oops

--- src/csp_firewall.c
+++ src/csp_firewall.c
@@ -3,14 +3,14 @@
 
 int csp_firewall_allow(csp_allow_list_t *entry, csp_packet_t *packet) {
 	while (entry) {
-		if ((packet->id.src & entry->src_mask) != (entry->src & entry->src_mask))
+		if (((packet->id.src & entry->src_mask) != (entry->src & entry->src_mask)) ||
+		    ((packet->id.dst & entry->dst_mask) != (entry->dst & entry->dst_mask)) ||
+		    ((packet->id.sport & entry->sport_mask) != (entry->sport & entry->sport_mask)) ||
+		    ((packet->id.dport & entry->dport_mask) != (entry->dport & entry->dport_mask))) {
+
+		    entry = entry->next;
 			continue;
-		if ((packet->id.dst & entry->dst_mask) != (entry->dst & entry->dst_mask))
-			continue;
-		if ((packet->id.sport & entry->sport_mask) != (entry->sport & entry->sport_mask))
-			continue;
-		if ((packet->id.dport & entry->dport_mask) != (entry->dport & entry->dport_mask))
-			continue;
+		}
 
 		// Match!
 		return 1;
